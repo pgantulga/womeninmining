cb7b90faa000c65d31618c711c10dd227e22fdd6
pgantulga <pggan2@gmail.com>
2021-11-06 11:26:06 +0800

    environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { MaterialModule } from './material/material.module';
+import { SharedModule } from './shared/shared.module';
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    SharedModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBS1rwYlPTy7vK7fi1nNTDOr0bgTPBziCg",
+    authDomain: "womeninmining-f8929.firebaseapp.com",
+    projectId: "womeninmining-f8929",
+    storageBucket: "womeninmining-f8929.appspot.com",
+    messagingSenderId: "449999203178",
+    appId: "1:449999203178:web:5172f188a1d61005745313",
+    measurementId: "G-RCRGCNX0NS"
+  }
 };
