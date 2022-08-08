bf2b5272e18190d587a68e353e3929229ae91ea6
tulgaaa <pggan2@gmail.com>
2022-08-08 18:36:00 +0800

    artcile-delete

--- src/app/core/admin/components/admin-articles/admin-articles.component.ts
+++ src/app/core/admin/components/admin-articles/admin-articles.component.ts
@@ -1,18 +1,56 @@
 import { Observable } from 'rxjs';
 import { ArticleService, Article } from './../../../services/article.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActionDialogComponent } from 'src/app/shared/components/action-dialog/action-dialog.component';
 
 @Component({
   selector: 'app-admin-articles',
   templateUrl: './admin-articles.component.html',
-  styleUrls: ['./admin-articles.component.scss']
+  styleUrls: ['./admin-articles.component.scss'],
 })
 export class AdminArticlesComponent implements OnInit {
-  articles$: Observable<Article[]>
-  constructor(private articleService: ArticleService) { }
+  articles$: Observable<Article[]>;
+  constructor(
+    private articleService: ArticleService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.articles$ = this.articleService.getArticles();
   }
-
+  deleteArticle(article) {
+    const dialogData = {
+      title: 'Adding article',
+      content: 'Your artcile will be removed from database.',
+    };
+    const dialogRef = this.dialog.open(ActionDialogComponent, {
+      data: dialogData,
+    });
+    return dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        return this.articleService.deleteArticle(article);
+      }
+    });
+  }
 }
+
+// const dialogData = !this.editing
+//   ? {
+//       title: 'Adding story',
+//       content: 'Your story will be added to database.',
+//     }
+//   : {
+//       title: 'Saving story',
+//       content: 'Your changes will be saved to database',
+//     };
+
+// return dialogRef.afterClosed().subscribe((result) => {
+//   if (result) {
+//     const dialogPromise = !this.editing ? this.addStory() : this.saveStory();
+//     dialogPromise.then(() => {
+//       console.log('done');
+//       return this.router.navigate(['admin/stories']);
+//     });
+//   }
+// });
